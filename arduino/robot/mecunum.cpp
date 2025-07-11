c90f5824561dfbbbd0f954e348215c0f8aca3cfd
batteryouo <145150404+batteryouo@users.noreply.github.com>
2025-07-12 03:31:51 +0800

    feat: add stop and movePolar methods for Mecanum drive (#2)

--- arduino/robot/mecunum.cpp
+++ arduino/robot/mecunum.cpp
@@ -29,6 +29,18 @@ void mec::Mecanum::move(float vx, float vy, float w){
     
 }
 
+void mec::Mecanum::stop() {
+    for (int i = 0; i < 4; ++i) {
+        _motor(motorPins[i].RPWM_OUTPUT, motorPins[i].LPWM_OUTPUT, 0);
+    }
+}
+
+void mec::Mecanum::movePolar(float speed, float angle, float w) {
+    float vx = speed * cos(angle);
+    float vy = speed * sin(angle);
+    move(vx, vy, w);
+}
+
 void mec::Mecanum::_motor(int pinR, int pinL, int speed) {
 
     int pwm = constrain(abs(speed), 0, 255);
@@ -51,14 +63,14 @@ void mec::Mecanum::_computeMecanumParam(float vx, float vy, float w, int *motor_
     float min_motor_w = -max_motor_w;
     
     float tmp_motor_w[4] = {0};
-    tmp_motor_w[0] = (vx - vy - length_x_y*w )/Mecanum_Wheel_Radius;
-    tmp_motor_w[1] = (vx + vy + length_x_y*w )/Mecanum_Wheel_Radius;
-    tmp_motor_w[2] = (vx + vy - length_x_y*w )/Mecanum_Wheel_Radius;
-    tmp_motor_w[3] = (vx - vy + length_x_y*w )/Mecanum_Wheel_Radius;
+    tmp_motor_w[0] = ( vx - vy - length_x_y*w )/Mecanum_Wheel_Radius;
+    tmp_motor_w[1] = ( vx + vy + length_x_y*w )/Mecanum_Wheel_Radius;
+    tmp_motor_w[2] = ( vx + vy - length_x_y*w )/Mecanum_Wheel_Radius;
+    tmp_motor_w[3] = ( vx - vy + length_x_y*w )/Mecanum_Wheel_Radius;
 
     // normalize
     for(int i = 0; i< 4; ++i){
-        float tmp_w = (tmp_motor_w[i] - min_motor_w) / ( max_motor_w - min_motor_w);
+        float tmp_w = (tmp_motor_w[i] - min_motor_w) / (max_motor_w - min_motor_w);
         motor_pwm[i] = (tmp_w - 0.5) * 255;
 
         motor_pwm[i] = motor_pwm[i] > 255? 255: motor_pwm[i];
